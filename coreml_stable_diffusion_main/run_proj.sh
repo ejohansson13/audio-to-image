e764858e6f59324109ef7e591ad17a5fe40b46ac
ejohansson13 <ejohansson@wisc.edu>
2023-10-10 16:57:44 -0700

    Updated run_proj

--- coreml_stable_diffusion_main/run_proj.sh
+++ coreml_stable_diffusion_main/run_proj.sh
@@ -1,17 +1,17 @@
-i=$(cat /Users/erik/Documents/ImageGenProject/coreml_stable_diffusion_main/version_num.txt)
-seed=$(cat /Users/erik/Documents/ImageGenProject/coreml_stable_diffusion_main/seed_num.txt)
+i=$(cat /PATH/TO/version_num.txt)
+seed=$(cat /PATH/TO/seed_num.txt)
 
-#ffmpeg -f avfoundation -i ":0" -t 10 /Users/erik/Documents/ImageGenProject/Recordings/test${i}.mkv
-#ffmpeg -i /Users/erik/Documents/ImageGenProject/Recordings/test${i}.mkv -ar 16000 -ac 1 -c:a pcm_s16le /Users/erik/Documents/ImageGenProject/Recordings/test${i}.wav
+ffmpeg -f avfoundation -i ":0" -ar 16000 -t 10 /PATH/TO/audio recording directory/test${i}.wav
 
-ffmpeg -f avfoundation -i ":0" -ar 16000 -t 10 /Users/erik/Documents/ImageGenProject/Recordings/test${i}.wav
+/PATH/TO/whisper.cpp-master/main -m /PATH/TO/whisper.cpp-master/models/ggml-medium.bin -nt -otxt -of /PATH/TO/txt file directory/test${i}.txt -f /PATH/TO/audio recording directory/test${i}.wav
 
-/Users/erik/Documents/ImageGenProject/whisper.cpp-master/main -m /Users/erik/Documents/ImageGenProject/whisper.cpp-master/models/ggml-medium.bin -nt -otxt -of /Users/erik/Documents/ImageGenProject/Text/test${i} -f /Users/erik/Documents/ImageGenProject/Recordings/test${i}.wav
+# multilingual option: specify language or let whisper detect it
+#/PATH/TO/whisper.cpp-master/main --language spanish -m /PATH/TO/whisper.cpp-master/models/ggml-large.bin -tr -nt -otxt -of /PATH/TO/txt file directory/test${i}.txt -f /PATH/TO/audio recording directory/test${i}.wav
 
-#/Users/erik/Documents/ImageGenProject/whisper.cpp-master/main --language spanish -m /Users/erik/Documents/ImageGenProject/whisper.cpp-master/models/ggml-large.bin -tr -nt -otxt -of /Users/erik/Documents/ImageGenProject/Text/test${i} -f /Users/erik/Documents/ImageGenProject/Recordings/test${i}.wav
+# remove_parentheses file removes any associated ({[]}) written through whisper's transcription
+python3 /PATH/TO/remove_parentheses.py
 
-python3 /Users/erik/Documents/ImageGenProject/coreml_stable_diffusion_main/remove_parentheses.py
+prompt=$(cat /PATH/TO/txt file directory/test${i}.txt)
 
-prompt=$(cat /Users/erik/Documents/ImageGenProject/Text/test${i}.txt)
-
-python3 -m python_coreml_stable_diffusion.pipeline --prompt "${prompt}" --compute-unit CPU_AND_GPU -o /Users/erik/Documents/ImageGenProject/SD_Images --seed ${seed} -i /Users/erik/Documents/ImageGenProject/coreml_stable_diffusion_main/models/coreml-stable-diffusion-2-1-base_original_packages/original/packages --model-version stabilityai/stable-diffusion-2-1-base --scheduler EulerAncestralDiscrete
+# when using split attention, CPU_AND_NE performance may be optimized
+python3 -m python_coreml_stable_diffusion.pipeline --prompt "${prompt}" --compute-unit CPU_AND_GPU -o /PATH/TO/image_output --seed ${seed} -i /PATH/TO/coreml_stable_diffusion_main/models/coreml-stable-diffusion-2-1-base_original_packages/original/packages --model-version stabilityai/stable-diffusion-2-1-base --scheduler EulerAncestralDiscrete
